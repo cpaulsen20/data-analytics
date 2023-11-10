8e9d6ba8d3107dfd0d7a3ec9a991304ec0850222
cpaulsen20 <149740432+cpaulsen20@users.noreply.github.com>
2023-11-09 20:45:26 -0600

    Update class_practice.py

--- test/class_practice.py
+++ test/class_practice.py
@@ -1,1 +1,15 @@
+# creating a new class called "shoePair"
+class = shoePair:
+  shoesize = 0
+  shoecolor = ""
+  shoetype = ""
+
+# creating a new object that will be classed as "shoepair"
+myNewShoe = shoepair()
+myNewShoe2 = shoepair()
+myNewShoe3 = shoepair()
+
+myNewShoe.size = 10
+myNewShoe.color = "blue"
+myNewShoe.type = "runner"
 
